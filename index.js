7d7ca20863c6b14d21c42ddc0e26cc263c3de237
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-10-26 02:48:56 -0400

    Use environment variable for setting port

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express();
 
+// First just check that PORT is set, otherwise exit
+if (!process.env.PORT) {
+  console.log("Error:  PORT env variable not set.");
+  process.exit(1);
+}
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 });
-

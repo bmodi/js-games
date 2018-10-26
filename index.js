38d52228e0111db1f85837e005157d7a573ec592
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-10-26 00:05:21 -0400

    Run express app with static pages

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+

17c438b71a6458e5cec35dca45c3c5a40e3751ff
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-10-26 03:06:00 -0400

    Removed hello world end point

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ if (!process.env.PORT) {
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}!`)
 });

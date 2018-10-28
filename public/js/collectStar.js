1c27beffe4e79b664ebc96c42da10c43e486ddae
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-10-27 22:21:02 -0400

    Removed unused file

--- public/js/collectStar.js
+++
@@ -1,9 +0,0 @@
-import { score, scoreText } from "./createGame.js";
-
-export default function collectStar (player, star)
-{
-    star.disableBody(true, true);
-
-    score += 10;
-    scoreText.setText('Score: ' + score);
-}

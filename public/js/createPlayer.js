5abebd6288c09f994a7fc4ac68e7e1525836fa41
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-10-28 00:01:20 -0400

    Moved createPlayer into module

---
+++ public/js/createPlayer.js
@@ -0,0 +1,23 @@
+export function createPlayer(parent) {
+    var newPlayer = parent.physics.add.sprite(100, 450, 'dude');
+    newPlayer.setBounce(0.2);
+    newPlayer.setCollideWorldBounds(true);
+    parent.anims.create({
+        key: 'left',
+        frames: parent.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    parent.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+    parent.anims.create({
+        key: 'right',
+        frames: parent.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    return newPlayer;
+}

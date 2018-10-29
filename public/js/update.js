5ee428acddd8fcc2de27bb88b1149d79c244d6e1
Mithu <bsmodi32@gmail.com>
2018-10-29 02:55:26 +0000

    Implement touch control for mobile

--- public/js/update.js
+++ public/js/update.js
@@ -2,13 +2,15 @@ import { player, cursors } from "./createGame.js";
 
 export default function update ()
 {
-    if (cursors.left.isDown)
+    if (cursors.left.isDown ||
+        (this.input.pointer1.isDown && this.input.pointer1.x<400))
     {
         player.setVelocityX(-160);
 
         player.anims.play('left', true);
     }
-    else if (cursors.right.isDown)
+    else if (cursors.right.isDown ||
+        (this.input.pointer1.isDown && this.input.pointer1.x>400))
     {
         player.setVelocityX(160);
 
@@ -21,7 +23,9 @@ export default function update ()
         player.anims.play('turn');
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
+    if ((cursors.up.isDown ||
+        (this.input.pointer1.isDown && this.input.pointer1.y<300))
+        && player.body.touching.down)
     {
         player.setVelocityY(-330);
     }

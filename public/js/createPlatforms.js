fa667ef56384aa64fecebd922012d164637c88f7
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-11-02 23:59:04 -0400

    Changed colour to brown

--- public/js/createPlatforms.js
+++ public/js/createPlatforms.js
@@ -5,9 +5,9 @@ export function createPlatforms(parent) {
     addBlock(0, 500, 800, 100, 0x00aa00);
 
     // Jumping platforms
-    addBlock(600, 250, 200, 50, 0xffff00);
-    addBlock(0, 175, 200, 50, 0xffff00);
-    addBlock(300, 350, 200, 50, 0xffff00);
+    addBlock(600, 250, 200, 50, 0x8B4513);
+    addBlock(0, 175, 200, 50, 0x8B4513);
+    addBlock(300, 350, 200, 50, 0x8B4513);
 
     return platforms;
 

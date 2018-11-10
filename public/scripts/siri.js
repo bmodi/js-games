edaed3b57564c7d5fff9f08b84476a065a13dc8a
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-11-09 21:05:38 -0500

    Moved siri code into js file

--- public/scripts/siri.js
+++ public/scripts/siri.js
@@ -11,3 +11,21 @@ function getSiriResponse(question) {
 
     return answer;
 }
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var inputValue = document.getElementById("myInput").value;
+    if (!inputValue) {
+      alert("You must write something!");
+    } else {
+      var li = document.createElement("li");
+      var t = document.createTextNode(inputValue);
+      li.appendChild(t);
+      document.getElementById("myUL").appendChild(li);
+  
+      var sli = document.createElement("li");
+      var st = document.createTextNode("SIRI:  "+getSiriResponse(inputValue));
+      sli.appendChild(st);
+      document.getElementById("myUL").appendChild(sli);
+    }
+  }

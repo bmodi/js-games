43d2e1b810f9fa0e04d62b772004bebdbef89bd0
Bhadresh Modi <bhadreshmodi@scotiabank.com>
2018-11-17 12:23:59 -0500

    Catch port in use error message and show error without stack trace.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const favicon = require('serve-favicon');
+const http = require('http');
+
 
 // First just check that PORT is set, otherwise exit
 if (!process.env.PORT) {
@@ -8,9 +10,16 @@ if (!process.env.PORT) {
   process.exit(1);
 }
 
+app.on("error", error => console.log("The server encountered an error!"));
+
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/images/favicon-96x96.png'));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}!`)
+http.createServer(app).listen( process.env.PORT, function(){
+  console.log("Server started listening on port " + process.env.PORT);
+})
+.on( 'error', function (e) { 
+  if (e.code == 'EADDRINUSE') { 
+    console.log('Cannot start server.  Port '+process.env.PORT+' already in use.');
+  }
 });

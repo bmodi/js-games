338cb96f1bd48052d66dc239528fac72f7e1e691
Rohan <rohan@PowerSvarb.local>
2019-07-16 00:16:34 -0400

    Added keyboard input

--- public/scripts/stars.js
+++ public/scripts/stars.js
@@ -4,12 +4,16 @@ var enemy;
 var stars;
 var bombs;
 var gems;
+var blueTeleporter;
+var redTeleporter;
 var gemsCollected = 0;
 var level = 1;
+var fireballs;
 
 var roundWon = false;
 var platforms;
 var cursors;
+var keyW;
 var score = 0;
 var scoreText;
 var gameStatusText;
@@ -24,6 +28,9 @@ var trailAllowed = true;
 var bombTrailAllowed = true;
 var numberOfStars = 1;
 var levelText;
+var requiredPointsText;
+var changeSpeed = true;
+var randomPlayerSpeed = 0;
 
 var enemyDestroyed = false;
 var assasinate = false;
@@ -58,9 +65,8 @@ function preload() {
     this.load.image('gem', 'images/gem.png');
     this.load.image('bomb', 'images/bomb.png');
     this.load.spritesheet('dude', 'images/dude.png', { frameWidth: 32, frameHeight: 48 });
-    if (level >= 3) {
-        this.load.spritesheet('enemy', 'images/invader.png', { frameWidth: 32, frameHeight: 32 });
-    }
+    this.load.spritesheet('enemy', 'images/invader.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('fireball', 'images/fireball.png');
 }
 
 function createScene()
@@ -71,15 +77,19 @@ function createScene()
     platforms = createPlatforms();
 
     cursors = scene.input.keyboard.createCursorKeys();
+    keyW = scene.input.keyboard.addKey('W');  // Get key object
+
     scene.input.addPointer(1);
     scoreText = scene.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
     levelText = scene.add.text(625, 16, 'level: 1', { fontSize: '32px', fill: '#000'});
+    requiredPointsText = scene.add.text(325, 16, score + '/' + level ** 2 * 100, { fontSize: '32px', fill: '#000'})
 
     scene.input.on('pointerdown', function (pointer) {
         if (gameOver) {
             resetScene();
             createSprites();
             gameOver=false;
+            roundWon = false;
         }
     }, scene);
 
@@ -88,33 +98,40 @@ function createScene()
 
 function createSprites() {
     player = createPlayer(scene);
-    if (level >= 3) {
-        enemy = createEnemy(scene);
-    }
     stars = createStars(scene);
     bombs = createBombs(scene);
     gems = createGems(scene);
+    fireballs = createFireballs(scene);
 
     scene.physics.add.collider(player, platforms);
     scene.physics.add.collider(stars, platforms);
     scene.physics.add.collider(bombs, platforms);
     scene.physics.add.collider(gems, platforms);
-    if (level >= 3) {
-        enemyCollider = scene.physics.add.collider(enemy, platforms);
-    }
     scene.physics.add.overlap(player, stars, collectStar, null, scene);
     scene.physics.add.collider(player, bombs, hitBomb, null, scene);
-    if (level >= 3) {
-        scene.physics.add.overlap(player, enemy, playerEnemyFight, null, scene);
-    }
     scene.physics.add.overlap(player, gems, powerUp, null, scene);
 
+    if (level >= 3 && roundWon) {
+        enemy = createEnemy(scene);
+        enemyCollider = scene.physics.add.collider(enemy, platforms);
+        scene.physics.add.overlap(player, enemy, playerEnemyFight, null, scene);
+    }
+    console.log("level is " + level + " before creating teleporters")
+    if (level >= 2 && roundWon) {
+        createTeleporter();
+        console.log("creating teleporters")
+    }
     scene.physics.resume();
+    console.log("sprites created for level "+level);
 }
 
 function resetScene() {
-    if (roundWon == false) {
-        level = 1;
+    console.log("reset function entered")
+    var previousLevel = level-1;
+    if (level >= 2 && roundWon == false) {
+        console.log("destorying teleporters!!!!!!!!");
+        blueTeleporter.destroy();
+        redTeleporter.destroy();
     }
     score=0;
     playerSpeed=160;
@@ -123,23 +140,26 @@ function resetScene() {
     stars.clear(true,true);
     bombs.clear(true,true);
     gems.clear(true,true);
-    if (level >= 4) {
+    if (level >= 3 && roundWon == false || level>=4) {
         enemy.destroy(true);
     }
     player.destroy(true);
     gemsCollected = 0;
     playerFrozen = false;
     jumpSpeed = 330;
-    if (level >= 3) {
-        scene.physics.add.collider(enemy, platforms);
+    if (level >= 4) {
+        badGem = Math.round( Math.random()*2 +2 )
     }
-    if (level>=2 && roundWon) {
-        createTeleporter();
-    }
-    roundWon = false;
-    levelText.setText('level: ' + level);
 
     numberOfStars = 1;
+    console.log("reset done for level "+previousLevel);
+    if (roundWon == false) {
+        level = 1;
+    }
+
+    requiredPointsText.setText(score + '/' + level ** 2 * 100)
+    levelText.setText('level: ' + level);
+    console.log("reset function finished. Level is " + level)
 } 
 
 function assasinationFailed() {
@@ -150,11 +170,10 @@ function resumePlayerSpeed() {
     playerSpeed = 160*1.15*badGem;
     jumpSpeed = 330*1.15*badGem;
     playerFrozen = false;
+    assasinate = true;
+
     if (level >= 3) {
         enemyWallPhase = false;
-    }
-    assasinate = true;
-    if (level >= 3) {
         scene.physics.add.collider(enemy, platforms);
             if (enemy.x > 300 && enemy.x < 400 && enemy.y > 51 && enemy.y < 226) {
                 enemy.y = 325;
@@ -165,52 +184,61 @@ function resumePlayerSpeed() {
                     }
             }
     }
-        setTimeout (assasinationFailed, 3000)
+
+    setTimeout (assasinationFailed, 3000)
 
     return playerSpeed
 }
 
 function powerUp(player, gem) {
     gem.destroy();
-    score += 10;
+    if (level >= 5) {
+        score -= level*5;
+    } else if (level == 4) {
+        score = score
+    } else {
+        score+=20
+    }
     scoreText.setText('Score: ' + score);
+    requiredPointsText.setText(score + '/' + level ** 2 * 100);
     gemsCollected = gemsCollected+1;
 
     if (gemsCollected == badGem && level>=4) {
         playerSpeed = 0;
         jumpSpeed = 0;
         playerFrozen = true;
-        if (level >= 3) {
-            enemyWallPhase = true;
-            enemyCollider.destroy();
-        }
+        enemyWallPhase = true;
+        enemyCollider.destroy();
         setTimeout (resumePlayerSpeed, 2000);
     } else {
         playerSpeed=playerSpeed*1.15;
         jumpSpeed=jumpSpeed*1.15;
+        console.log("playerSpeed is " + playerSpeed);
     }
-        if (gemsCollected >= numberOfGems) {
-            score = score*2
-            scoreText.setText('Score: ' + score);
-                if (score>=level ** 2 *100) {
-                    gameStatusText = this.add.text(75, 200, 'congratulations, you win!', { fontSize: '45px', fill: '#000' });
-                    gameOver = true;
-                    ++level;
-                    roundWon = true;
-                } else {
-                    gameStatusText = this.add.text(50, 200, 'Your a complete failure. Try again.', { fontSize: '25px', fill: '#000' });
-                    gameOver = true;
-                }
-        } else if (gemsCollected >= numberOfGems - 1) {
-            player.y = 100
-            player.x = 350
-        }
+    if (gemsCollected >= numberOfGems) {
+        scoreText.setText('Score: ' + score);
+            if (score>=level ** 2 *100) {
+                gameStatusText = this.add.text(75, 200, 'congratulations, you win!', { fontSize: '45px', fill: '#000' });
+                gameOver = true;
+                ++level;
+                roundWon = true;
+                this.physics.pause();
+            } else {
+                gameStatusText = this.add.text(150, 200, 'Your a complete failure. Try again.', { fontSize: '25px', fill: '#000' });
+                gameOver = true;
+                this.physics.pause();
+            }
+    } else if (gemsCollected >= numberOfGems - 1) {
+        player.y = 100
+        player.x = 350
+    }
 }
 
 function collectStar(player, star) {
     star.destroy();
-    score += 10;
+    score += 20;
     scoreText.setText('Score: ' + score);
+    requiredPointsText.setText(score + '/' + level ** 2 * 100)
 
     var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
 
@@ -221,8 +249,7 @@ function collectStar(player, star) {
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
         bomb.allowGravity = false;
-        console.log ("numberOfStars is " + numberOfStars);
-        }
+    }
 
     ++numberOfStars;
     if (numberOfStars >= 5) {
@@ -235,8 +262,6 @@ function collectStar(player, star) {
     star.setCollideWorldBounds(true);
     star.setVelocity(Phaser.Math.Between(-200, 200), 20);
     star.allowGravity = false;
-
-    console.log("numberOfStars is " + numberOfStars);
 }
 
 function hitBomb(player, bomb) {
@@ -247,7 +272,6 @@ function hitBomb(player, bomb) {
     gameOver = true;
 }
 
-if (level >= 3) {
 function playerEnemyFight(player, enemy) {
     if (assasinate == false) {
         this.physics.pause();
@@ -260,7 +284,6 @@ function playerEnemyFight(player, enemy) {
         score += 100;
         scoreText.setText('Score: ' + score);
         enemyDestroyed = true;
-    }
     }
 }
 
@@ -301,6 +324,11 @@ function createGems(parent) {
     return gems;
 }
 
+function createFireballs (parent) {
+    fireballs = parent.physics.add.group();
+    return fireballs;
+}
+
 function createBombs(parent) {
     bombs = parent.physics.add.group();
     return bombs;
@@ -330,40 +358,37 @@ function createPlayer(parent) {
     return player;
 }
 
-if (level >= 3) {
-    function createEnemy(parent) {
-        var enemy = parent.physics.add.sprite(700, 100, 'enemy');
-        enemy.setBounce(0.2);
-        enemy.setCollideWorldBounds(true);
-        parent.anims.create({
-            key: 'default',
-            frames: parent.anims.generateFrameNumbers('enemy', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        return enemy;
-    }
+function createEnemy(parent) {
+    var enemy = parent.physics.add.sprite(700, 100, 'enemy');
+    enemy.setBounce(0.2);
+    enemy.setCollideWorldBounds(true);
+    parent.anims.create({
+        key: 'default',
+        frames: parent.anims.generateFrameNumbers('enemy', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    return enemy;
+}
 
+function starTrail() {
+    var star = stars.create(enemy.x, enemy.y, 'star');
+    star.setBounce(1);
+    star.setCollideWorldBounds(true);
+    star.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    star.allowGravity = true;
+    
+    trailAllowed = true;
+}
 
-    function starTrail() {
-        var star = stars.create(enemy.x, enemy.y, 'star');
-        star.setBounce(1);
-        star.setCollideWorldBounds(true);
-        star.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        star.allowGravity = true;
-        
-        trailAllowed = true;
-    }
+function bombTrail() {
+    var bomb = bombs.create(enemy.x, enemy.y, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = true;
 
-    function bombTrail() {
-        var bomb = bombs.create(enemy.x, enemy.y, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = true;
-
-        bombTrailAllowed = true;
-    }
+    bombTrailAllowed = true;
 }
 
 function createPlatforms() {
@@ -389,13 +414,16 @@ function createPlatforms() {
 }
 
 function addBlock(startX, startY, width, height, color) {
-    platforms.add(scene.add.rectangle(startX + width / 2, startY + height / 2, width, height, color));
+    var newRectangle = scene.add.rectangle(startX + width / 2, startY + height / 2, width, height, color);
+    platforms.add(newRectangle);
+    return newRectangle;
 }
 
 function createTeleporter() {
-    addBlock(50, 495, 30, 5, 0x3333ff); //blue teleporter
-    addBlock(675, 495, 30, 5, 0xff0000); //red teleporter
-    }
+    blueTeleporter = addBlock(50, 495, 30, 5, 0x3333ff); //blue teleporter
+    redTeleporter = addBlock(675, 495, 30, 5, 0xff0000); //red teleporter
+    console.log("createTeleporter function entered")
+}
 
 function update() {
     if (!gameOver) {
@@ -441,9 +469,9 @@ function update() {
                     setTimeout(starTrail, 200);
                 }
 
-                if (playerFrozen && bombTrailAllowed) {
+                if (playerFrozen && bombTrailAllowed && level >= 5) {
                     bombTrailAllowed = false;
-                    setTimeout (bombTrail, 2000);
+                    setTimeout (bombTrail, 2500 - level*100);
                 }
 
                 if (player.y < enemy.y && playerFrozen) {
@@ -458,6 +486,13 @@ function update() {
             }
         }
 
+        // if (score >= 40 && level <= 4) {
+        //     gameStatusText = this.add.text(75, 200, 'Wow, you won the test round!', { fontSize: '45px', fill: '#000' });
+        //         gameOver = true;
+        //         ++level;
+        //         roundWon = true;
+        // }
+
         if (level >= 2) {
             if (player.x > 50 && player.x < 80 && player.y < 495 && player.y > 470) { //blue teleporter
                 player.y = 225;
@@ -470,22 +505,40 @@ function update() {
             }
         }
 
-        if (level <= 2 && score >= 10) {
-            gameStatusText = this.add.text(75, 200, 'congratulations, you win!', { fontSize: '45px', fill: '#000' });
-            gameOver = true;
-            ++level;
-            roundWon = true;
+        function allowSpeedChange() {
+            changeSpeed = true
+        }
+
+        function getRandomPlayerSpeed() {
+            randomPlayerSpeed = Number (Math.round( Math.random()*100*level + 160   ))
+            console.log("randomPlayerSpeed is " + randomPlayerSpeed);
+            console.log("playerSpeed is " + playerSpeed)
+            var nextSpeedChangeTimer = Math.round( Math.random()*250 + 250)
+            setTimeout(allowSpeedChange, nextSpeedChangeTimer);
+            return randomPlayerSpeed
         }
 
         // Player control based on left/right/jump keys
         if (cursors.left.isDown ||
             (this.input.pointer1.isDown && this.input.pointer1.x < 400)) {
-            player.setVelocityX(-playerSpeed);
+                if (level >= 6 && changeSpeed) {
+                    changeSpeed = false
+                    playerSpeed = getRandomPlayerSpeed()
+                    console.log("playerSpeed is " + playerSpeed);
+                    player.setVelocityX(-playerSpeed)
+            } else {
+                player.setVelocityX(-playerSpeed);
+            }
             player.anims.play('left', true);
         } else if (cursors.right.isDown ||
             (this.input.pointer1.isDown && this.input.pointer1.x > 400)) {
-            player.setVelocityX(playerSpeed);
-            player.anims.play('right', true);
+                if (level >= 6 && changeSpeed) {
+                    changeSpeed = false;
+                    playerSpeed = getRandomPlayerSpeed()
+                    player.setVelocityX( playerSpeed  )
+                } else {
+                    player.setVelocityX(playerSpeed);
+                }            player.anims.play('right', true);
         } else {
             player.setVelocityX(0);
             player.anims.play('turn');
@@ -496,11 +549,21 @@ function update() {
             && player.body.touching.down) {
             player.setVelocityY(-jumpSpeed);
         } else if (cursors.down.isDown && !cursors.up.isDown && !cursors.left.isDown && !cursors.right.isDown) {
+            player.setVelocityY(jumpSpeed)
             player.setTint(0x00ff00);
+            // if (score == 0) {
+            //     blueTeleporter.destroy();
+            //     redTeleporter.destroy();
+            //     console.log("teleporters destroyed")
+            // }
         } else if (assasinate && level >= 3) {
             player.setTint(0xff0000);
         } else if (playerFrozen) {
             player.setTint(0xccff);
         } else player.setTint(0xffffff);
+
+        if (keyW.isDown) {
+            player.setTint(0xff0000);
+        }
     }
 }

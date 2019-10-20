e39908734cafd4fa071b31603f838823870875b1
Bhadresh Modi <BModi@casebank.com>
2019-10-20 13:01:37 -0400

    Moved siri math functions into a separate file

--- public/scripts/siri.js
+++ public/scripts/siri.js
@@ -79,39 +79,6 @@ function startJoke() {
     return joke
 }
 
-function findSum(value) {
-    var total=0;
-        for (var i=0; i<=value; i++) {
-            total = total+i
-            console.log ("the total is " + total);
-            console.log ("i is " + i);
-        }
-        return total
-}
-
-function findFactor(factorValue) {
-    var product=1;
-        for (var j=1; j<=factorValue; j++) {
-            product = product*j
-            console.log("j is " + j);
-            console.log("product is " + product);
-        }
-        return product
-}
-
-function findFahrenheit(degreesC) {
-    var fahrenheitDegrees= 0;
-    fahrenheitDegrees = (degreesC *9/5) + 32
-    console.log("degreesC is " + degreesC);
-    return fahrenheitDegrees
-}
-
-function findCelsius(degreesF) {
-    var celsiusDegrees = 0;
-    celsiusDegrees = (degreesF - 32) * 5/9;
-    return celsiusDegrees
-}
-
 function reverseQuestion(reverseString) {
     var reversedQuestion = "";
         for (var k=0; k <= reverseString.length; k++) {
